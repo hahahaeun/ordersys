d64a98ddca33cfb158e650158f52a5f1827029a2
hahahaeun <rlagkdmsdlek@gmail.com>
2024-06-27 12:37:06 +0900

    FEAT : add file

---
+++ src/main/java/com/example/ordersys/domain/product/Product.java
@@ -0,0 +1,44 @@
+package com.example.ordersys.domain.product;
+
+import com.example.ordersys.domain.product.exception.SoldOutException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class Product {
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(name = "stock", nullable = false)
+    private int stock;
+
+    public Product(Long id, String name, int price, int stock){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+    public void decreaseStock(int quantity) {
+        if (this.stock < quantity) {
+            throw new SoldOutException("SoldOutException 발생. 주문한 상품량이 재고량보다 큽니다.");
+        }
+        this.stock -= quantity;
+    }
+
+}
